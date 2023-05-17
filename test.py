8231404b60661ebc4071ea50092bfa1de6e2df96
Samar Khanna <samar.khanna@gmail.com>
2023-05-17 07:39:14 -0700

    Fix for test

--- test.py
+++ test.py
@@ -36,11 +36,18 @@ from data.load_data import load_data
 from models import create_model
 from util.visualizer import save_images
 from util import html
+from omegaconf import OmegaConf
 
 try:
     import wandb
 except ImportError:
     print('Warning: wandb package cannot be found. The option "--use_wandb" will result in error.')
+
+
+def load_vae(config_path):
+    config = OmegaConf.load(config_path)
+    from autoencoder.autoencoder import AutoencoderKL
+    return AutoencoderKL(**config['model']["params"])
 
 
 if __name__ == '__main__':
@@ -62,6 +69,9 @@ if __name__ == '__main__':
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
 
+    if opt.use_vae:
+        vae = load_vae('autoencoder/ldm_v21_vae.yaml')
+
     # initialize logger
     if opt.use_wandb:
         wandb_run = wandb.init(project=opt.wandb_project_name, name=opt.name, config=opt) if not wandb.run else wandb.run
@@ -81,9 +91,7 @@ if __name__ == '__main__':
 
     generated = []
     reference = []
-    for i, data in enumerate(val_dataset):
-        if i >= opt.num_test:  # only apply our model to opt.num_test images.
-            break
+    for i, data in enumerate(dataset):
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
@@ -92,12 +100,24 @@ if __name__ == '__main__':
             print('processing (%04d)-th image... %s' % (i, img_path))
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize, use_wandb=opt.use_wandb)
 
-        for label, im_data in visuals.items():
-            generated.append(util.util.tensor2im(im_data))
-            reference.append(data['A'])
+        gt = visuals['real_B']
+        if opt.use_vae:
+            gt = vae.decode(gt)
+        reference.append(util.util.tensor2im(gt))
+
+        gen = visuals['fake_B']
+        if opt.use_vae:
+            gen = vae.decode(gen)
+        generated.append(util.util.tensor2im(gen))
+
+        # for label, im_data in visuals.items():
+        #     gt = data['A']
+        #     gen = im_data
+        #     generated.append(util.util.tensor2im(im_data))
+        #     reference.append(util.util.tensor2im(data['A']))
 
     generated = np.stack(generated)
-    np.savez(os.path.join(opt.results_dir, 'generated.npz'), generated)
+    np.savez(os.path.join(opt.results_dir, opt.name, 'generated.npz'), generated)
     reference = np.stack(reference)
-    np.savez(os.path.join(opt.results_dir, 'reference.npz'), reference)
+    np.savez(os.path.join(opt.results_dir, opt.name, 'reference.npz'), reference)
     webpage.save()  # save the HTML
